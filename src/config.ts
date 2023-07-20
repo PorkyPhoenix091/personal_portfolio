881e8d45e0b8c4c96dfdeeb6ce9ef9cb334268f0
Anthony_ <anthonygiuliano59@gmail.com>
2023-07-20 17:39:45 +0200

    updated config and main page

--- src/config.ts
+++ src/config.ts
@@ -1,11 +1,11 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Captain Awesome";
+export const SITE_TITLE = "Anthony Gerardo Giuliano";
 export const SITE_DESCRIPTION =
-  "Welcome to my blog! I write about modern web development.";
-export const TWITTER_HANDLE = "@yourtwitterhandle";
-export const MY_NAME = "Captain Awesome";
+  "Welcome to my dev Portfolio and Blog! I write about modern web development and cybersecurity";
+export const TWITTER_HANDLE = "@Anthony091_";
+export const MY_NAME = "Anthony Gerardo Giuliano";
 
 // setup in astro.config.mjs
 const BASE_URL = new URL(import.meta.env.SITE);
